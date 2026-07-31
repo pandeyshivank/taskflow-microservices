cf14ca94b5f7386fc23555aa70eb43c9ab9719a2
pandeyshivank <pandeyshivank@gmail.com>
2026-07-30 20:55:03 -0500

    implementated rate limiting and load balancing in API gateway

--- Backend/TaskFlowServices.ApiGateway/Program.cs
+++ Backend/TaskFlowServices.ApiGateway/Program.cs
@@ -1,3 +1,6 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -5,7 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("FixedPolicy", opt =>
+    {
+        opt.PermitLimit = 10;
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.QueueLimit = 0;
+    });
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,7 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
    
 }
-
+app.UseRateLimiter();
 app.UseHttpsRedirection();
 app.MapReverseProxy();
 app.UseCors("MyDefultpolicy");
